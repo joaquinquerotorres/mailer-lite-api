4f0748f01c302446f0e338e90102fdde0e4de5b6
Joaquín Quero Torres <joaquinquerotorres@MacBook-Air-de-Joaquin.local>
2026-08-14 11:58:53 +0200

    Add urls to expose them

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Campaign\Infrastructure\Controllers\CreateCampaignController;
 use App\Campaign\Infrastructure\Controllers\GetCampaignController;
 use App\Campaign\Infrastructure\Controllers\GetCampaignsController;
+use App\Campaign\Infrastructure\Controllers\UpdateCampaignController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/campaigns', GetCampaignsController::class);
 Route::get('/campaigns/{campaignUuid}', GetCampaignController::class);
+Route::post('/campaigns', CreateCampaignController::class);
+Route::put('/campaigns/{campaignUuid}', UpdateCampaignController::class);
